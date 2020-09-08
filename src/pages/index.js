9a052bf107e39ea6cb3abd7ced212be09bd2c9d8
Tim Abrahamson <tim@kinetic.com>
2020-09-08 15:14:58 -0500

    Building the basics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Layout } from "../components/Layout";
 
 export default () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 };
