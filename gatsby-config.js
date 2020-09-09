8e9330048e792eba6e6c2a5dd6ed819f12d1aec9
Tim Abrahamson <tim@kinetic.com>
2020-09-08 23:08:22 -0500

    Added simple markup styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,40 @@
 const siteMetadata = {
-  title: `A Developer Called Tims' Blog`,
-  description: `This is my coding blog where I write about my coding journey.`
+  title: `A Developer Called Tim`,
+  description: `This is my coding blog where I write about my coding journey.`,
+  image: `/default-site-image.jpg`,
+  siteUrl: `https://timabrahamson.dev`,
+  siteLanguage: `en-US`,
+  siteLocale: `en_us`,
+  twitterUsername: `@timabrahamson1`,
+  authorName: `Tim Abrahamson`
 };
 
 module.exports = {
   siteMetadata: siteMetadata,
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`.mdx`, `.md`]
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ]
       }
     },
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,76 @@
+import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
 import React from "react";
+import styled from "styled-components";
 import { Layout } from "../components/Layout";
+// import Dump from "../components/Dump";
+// import { useSiteMetadata } from "../hooks/useSiteMetadata";
 
-export default () => {
+const IndexWrapper = styled.main``;
+
+const PostWrapper = styled.div``;
+
+const Image = styled(Img)`
+  border-radius: 5px;
+`;
+
+export default ({ data }) => {
+  // const {
+  //   description,
+  //   title,
+  //   image,
+  //   siteUrl,
+  //   siteLanguage,
+  //   siteLocale,
+  //   twitterUsername
+  // } = useSiteMetadata();
+
   return (
-    <>
-      <Layout />
-    </>
+    <Layout>
+      <IndexWrapper>
+        {/* <Dump
+          title={title}
+          description={description}
+          image={`${siteUrl}${image}`}
+          pathname={siteUrl}
+          siteLanguage={siteLanguage}
+          siteLocale={siteLocale}
+          twitterUsername={twitterUsername}
+        /> */}
+        {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
+          <PostWrapper key={id}>
+            <Link to={fields.slug}>
+              {!!frontmatter.cover ? (
+                <Image sizes={frontmatter.cover.childImageSharp.sizes} />
+              ) : null}
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </Link>
+          </PostWrapper>
+        ))}
+      </IndexWrapper>
+    </Layout>
   );
 };
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date(formatString: "YYYY MMMM Do")
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`;
